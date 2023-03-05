2a4bce7cf52545959f20e9ea86475dbdee9d58d4
psaghafi <psaghafi@sfu.ca>
2023-03-04 17:20:10 -0800

    zombie run

---
+++ Assets/StarterAssets/FirstPersonController/Scenes/zombieRigidBodyScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class zombieRigidBodyScript : MonoBehaviour
+{
+    public Transform GameObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3
+    }
+}
